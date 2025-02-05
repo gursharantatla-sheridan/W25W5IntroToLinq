e2cec594668503cf386faf0d67e732b4162090d8
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-05 16:20:06 -0500

    linq ex

--- W25W5IntroToLinq/Program.cs
+++ W25W5IntroToLinq/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 5, 3, 4, 6, 7, 4, 9, 2, 8, 7, 4, 1 };
+
+            var result = from n in array
+                         where n > 4
+                         orderby n
+                         select n;
+
+            foreach (var i in result)
+                Console.Write(i + " ");
+            Console.WriteLine("\n\n");
+
+
         }
     }
 }
