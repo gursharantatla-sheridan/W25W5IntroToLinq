bdfef72208847c0d980aa0eca551d85236fafd9f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-05 16:38:02 -0500

    linq

---
+++ W25W5IntroToLinq/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W25W5IntroToLinq
+{
+    public class Employee
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public double Salary { get; set; }
+
+        public Employee(string firstName, string lastName, double salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+
+
+    }
+}
--- W25W5IntroToLinq/Program.cs
+++ W25W5IntroToLinq/Program.cs
@@ -6,16 +6,50 @@
         {
             int[] array = { 5, 3, 4, 6, 7, 4, 9, 2, 8, 7, 4, 1 };
 
+
+            //query syntax
             var result = from n in array
                          where n > 4
                          orderby n
                          select n;
+
+
+            // method syntax
+            //var result = array.Where(n => n > 4);
+
 
             foreach (var i in result)
                 Console.Write(i + " ");
             Console.WriteLine("\n\n");
 
 
+
+            List<string> colors = new List<string>();
+            colors.Add("BlUe");
+            colors.Add("rUsT");
+            colors.Add("GreEN");
+            colors.Add("ReD");
+
+            var startsWithR = from c in colors
+                              let uppercaseColors = c.ToUpper()
+                              where uppercaseColors.StartsWith("R")
+                              orderby uppercaseColors
+                              select uppercaseColors;
+
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
+
+
+
+            // deferred execution
+
+            colors.Add("rUBy");
+            colors.Add("PInk");
+
+            foreach (var i in startsWithR)
+                Console.WriteLine(i);
+            Console.WriteLine("\n\n");
         }
     }
 }
